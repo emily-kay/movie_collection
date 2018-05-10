ec744ec12c047aacb5bbe2ac770daff66095941c
Emily Kay <emilykay917@gmail.com>
2018-05-10 15:47:44 -0500

    Corrected which tab the movies appear on

--- server/public/scripts/controllers/home.controller.js
+++ server/public/scripts/controllers/home.controller.js
@@ -4,4 +4,21 @@ app.controller('HomeController', ['$http', function($http){
     console.log('HomeController works');
     var self = this;
     self.message = "A Place to Keep All Your Favorite Films"
+    self.allMovies = {
+        details: []
+    };
+
+    self.getAllMovies = function(){
+        $http({
+        method: 'GET',
+        url: '/movies',
+    })
+    .then(function(response){
+        self.allMovies.details = response.data;
+    })
+    .catch(function(error){
+        console.log('No homes for you ', error)
+    })}; //end GET
+
+    self.getAllMovies();
 }]);//end HomeController
