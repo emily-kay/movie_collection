ec6b12a7b533a10ad57d0bcecb1e335bb2350219
Emily Kay <emilykay917@gmail.com>
2018-05-11 17:30:44 -0500

    Fixed the POST for genres and images

--- server/routes/movies.router.js
+++ server/routes/movies.router.js
@@ -7,15 +7,28 @@ const pool = require('../modules/database')
 
 //---------------GET and POST from Movie Controller----------------//
 
+// router.get('/', (req,res)=>{
+//     pool.query(`SELECT * FROM "movies" FULL OUTER JOIN "genres"
+//                 ON "movies"."genre_id" = "genres"."id";`)
+//         .then((results)=>{
+//             res.send(results.rows);
+//         })
+//         .catch((error)=>{
+//             console.log('error with SQL SELECT on get ', error);
+//             res.sendStatus(500);
+//         });
+// }); //end GET
+
 router.get('/', (req,res)=>{
-    pool.query (`SELECT "m"."name",
+    pool.query(`SELECT "m"."name",
                         "m"."id",
                         "m"."director",
                         "m"."release_date",
                         "m"."run_time",
-                        "g"."name"
-                FROM "movies" as "m" JOIN "genres" as "g"
-                ON "m"."genre_id" = "g"."id";`)
+                        "m"."image_path",
+                        "g"."name" as "genre_name"
+                    FROM "movies" as "m" JOIN "genres" as "g"
+                    ON "m"."genre_id" = "g"."id";`)
         .then((results)=>{
             res.send(results.rows);
         })
@@ -27,8 +40,8 @@ router.get('/', (req,res)=>{
 
 router.post('/', (req, res)=>{
     const movie = req.body;
-    pool.query(`INSERT INTO "movies" ("name", "director",  "release_date", "run_time")
-    VALUES ($1, $2, $3, $4);`, [movie.name, movie.director, movie.release_date, movie.run_time])
+    pool.query(`INSERT INTO "movies" ("name", "director",  "release_date", "run_time", "genre_id")
+    VALUES ($1, $2, $3, $4, $5);`, [movie.name, movie.director, movie.release_date, movie.run_time, movie.genre_id])
         .then((results)=>{
             console.log(results);
             
