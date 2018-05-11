3fbf6c88eb56309ca4bf93f227ce5e803111cf75
Emily Kay <emilykay917@gmail.com>
2018-05-11 14:02:38 -0500

    Submit tab grabs only the most recently submitted movie

---
+++ server/routes/recentMovie.router.js
@@ -0,0 +1,23 @@
+//---------------Requirements----------------//
+
+const express = require('express');
+const pg = require('pg');
+const router = express.Router();
+const pool = require('../modules/database')
+
+//---------------GET and POST from Movie Controller----------------//
+
+router.get('/', (req,res)=>{
+    pool.query(`SELECT * FROM "movies"
+        ORDER BY "movies"."id" DESC
+        LIMIT 1;`)
+        .then((results)=>{
+            res.send(results.rows);
+        })
+        .catch((error)=>{
+            console.log('error with SQL SELECT on get ', error);
+            res.sendStatus(500);
+        });
+}); //end GET
+
+module.exports = router;
